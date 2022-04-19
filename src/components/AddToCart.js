2e8578c1d4c78763114a60cccc230da192247803
SG <enyacat@gmail.com>
2022-04-19 23:29:54 +1000

    addtocart

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -6,7 +6,51 @@ import { useCartContext } from '../context/cart_context'
 import AmountButtons from './AmountButtons'
 
 const AddToCart = () => {
-  return <h4>addToCart </h4>
+  const { id, stock, colors } = product
+  const [mainColor, setMainColor] = useState(colors[0])
+
+  const increase = () => {
+    setAmount((oldAmount) => {
+      let tempAmount = oldAmount + 1
+      if (tempAmount > stock) {
+        tempAmount = stock
+      }
+      return tempAmount
+    })
+  }
+
+  const decrease = () => {
+    setAmount((oldAmount) => {
+      let tempAmount = oldAmount - 1
+      if (tempAmount < 1) {
+        tempAmount = 1
+      }
+      return tempAmount
+    })
+  }
+
+  return (
+    <Wrapper>
+      <div className='colors'>
+        <span> colors : </span>
+        {colors.map((color, index) => {
+          return (
+            <button
+              key={index}
+              style={{ background: color }}
+              className={`${
+                mainColor === color ? 'color-btn active' : 'color-btn'
+              }`}
+              onClick={() => setMainColor(color)}
+            >
+              {mainColor === color ? <FaCheck /> : null}
+            </button>
+          )
+        })}
+      </div>
+      <div className='btn-container'></div>
+    </Wrapper>
+  )
 }
 
 const Wrapper = styled.section`
